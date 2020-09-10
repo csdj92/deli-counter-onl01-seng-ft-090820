7ced21979abac5070f4e87257d3b7e6de5030e1b
CJ Davis <csdj92@gmail.com>
2020-09-10 15:06:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     #line with guest
     guest_line=[]
     guest = 1
-    line_number.each.with_index()do |guest, order|
+    array.each.with_index()do |guest, order|
       guest_line.push("#{order}. #{guest}")
     end
     puts "The line is currently: #{guest_line.join} (" ")"
