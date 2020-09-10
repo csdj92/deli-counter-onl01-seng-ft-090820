4f047ded3afbdf0cc030f07279aeab1801685932
CJ Davis <csdj92@gmail.com>
2020-09-10 14:46:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(line_number)
   if line_number = 0
     puts "The line is currently empty."
-  else line_number > 0
+  else line_number > 1
     puts "The line is currently: #{line_number}"
 end
 end
